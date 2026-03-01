355176df3a33a51c960b28592c479d3a8f77622e
LobbyTech <1187755698@qq.com>
2026-03-02 02:39:59 +0800

    Update BlockPopulator.java

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/generations/BlockPopulator.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/generations/BlockPopulator.java
@@ -176,16 +176,16 @@ public class BlockPopulator extends org.bukkit.generator.BlockPopulator {
             	try {
             		BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController();
 
-            		
-            		if (location.getBlock().getType() != Material.AIR) {
-            			location.getBlock().setType(Material.AIR, false);
-            		}
-            		    
+            		Bukkit.getScheduler().runTask(RykenSlimefunCustomizer.INSTANCE, () -> {
 
+                		if (controller.getBlockData(location) != null) {
+                		    controller.removeBlock(location);
+                		}
+                		if (location.getBlock().getType() != Material.AIR) {
+                			location.getBlock().setType(Material.AIR, false);
+                		}
+            		});
             		
-            		if (controller.getBlockData(location) != null) {
-            		    controller.removeBlock(location);
-            		}
                 		
             		if (location.getBlock().getType() == Material.AIR && controller.getBlockData(location) == null) {
             			controller.createBlock(location, generationInfo.getSlimefunItemStack().getItemId());
