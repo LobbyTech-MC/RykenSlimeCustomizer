209eea8c05fd48139929f2d354563016c60626a9
LobbyTech <1187755698@qq.com>
2026-03-08 23:34:25 +0800

    使用FAWE优化生成

--- src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/generations/BlockPopulator.java
+++ src/main/java/org/lins/mmmjjkx/rykenslimefuncustomizer/objects/customs/generations/BlockPopulator.java
@@ -20,15 +20,21 @@ import org.lins.mmmjjkx.rykenslimefuncustomizer.objects.ProjectAddon;
 import org.lins.mmmjjkx.rykenslimefuncustomizer.objects.Range;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+import com.sk89q.jnbt.StringTag;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.blocks.BaseItemStack;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.block.BaseBlock;
+import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockType;
+import com.sk89q.worldedit.world.block.BlockTypes;
 import com.sk89q.worldedit.world.item.ItemType;
 import com.xzavier0722.mc.plugin.slimefun4.storage.controller.BlockDataController;
+import com.sk89q.jnbt.CompoundTag;
+import com.sk89q.jnbt.ListTag;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
@@ -84,18 +90,7 @@ public class BlockPopulator extends org.bukkit.generator.BlockPopulator {
 	    return block;
 	}
 	
-	private static BaseItemStack createSkullBaseBlock(PlayerProfile profile) {
-        // 创建临时头颅物品获取NBT
-        ItemStack skullItem = new ItemStack(Material.PLAYER_HEAD);
-        SkullMeta meta = (SkullMeta) skullItem.getItemMeta();
-        
-        // 设置玩家皮肤配置
-        meta.setOwnerProfile(profile);
-        skullItem.setItemMeta(meta);
-        
-        // 转换为FAWE的BaseBlock
-        return BukkitAdapter.adapt(skullItem);
-    }
+
 
 
 
@@ -134,75 +129,119 @@ public class BlockPopulator extends org.bukkit.generator.BlockPopulator {
             @Nonnull Random random,
             @Nonnull GenerationInfo generationInfo,
             @Nonnull GenerationArea area) {
-        Range height = area.getHeight();
-        int h = height.getDistance() + 1;
-        int r;
+    	Bukkit.getScheduler().runTaskAsynchronously(RykenSlimefunCustomizer.INSTANCE, () -> {
+    		Range height = area.getHeight();
+            int h = height.getDistance() + 1;
+            int r;
 
-        if (h < 0) {
-            h = 1;
-        }
+            if (h < 0) {
+                h = 1;
+            }
 
-        double s2 = random.nextDouble(0, h);
+            double s2 = random.nextDouble(0, h);
 
-        double sTop = (height.max() - area.getMost() + 1);
-        if (s2 < sTop) {
-            int h2MaxHeight = (int) (s2 * 2);
-            r = height.max() - h2MaxHeight;
-        } else {
-            s2 -= sTop;
-            int h2MinHeight = (int) (s2 * 2);
-            r = height.min() + h2MinHeight;
-        }
+            double sTop = (height.max() - area.getMost() + 1);
+            if (s2 < sTop) {
+                int h2MaxHeight = (int) (s2 * 2);
+                r = height.max() - h2MaxHeight;
+            } else {
+                s2 -= sTop;
+                int h2MinHeight = (int) (s2 * 2);
+                r = height.min() + h2MinHeight;
+            }
 
-        int centerX = (chunkX << 4) + random.nextInt(16);
-        int centerY = r;
-        int centerZ = (chunkZ << 4) + random.nextInt(16);
-
-        Bukkit.getScheduler().runTaskAsynchronously(RykenSlimefunCustomizer.INSTANCE, () -> {
-        	
+            int centerX = (chunkX << 4) + random.nextInt(16);
+            int centerY = r;
+            int centerZ = (chunkZ << 4) + random.nextInt(16);
+            
+            com.sk89q.worldedit.world.World faweworld = BukkitAdapter.adapt(world);
+        	//EditSession editSession = WorldEdit.getInstance().newEditSession(faweworld);
         	try (EditSession editSession = WorldEdit.getInstance().newEditSessionBuilder()
+        			.world(faweworld)
                     .maxBlocks(-1)
                     .fastMode(true)
                     .build()) {
             	
-                for (int i = 0; i < area.getSize().getRandomBetween(random); i++) {
-                    Location location = new Location(world, centerX, centerY, centerZ);
-                    Block block = world.getBlockAt(centerX, centerY, centerZ);
-                    if (!(centerX >= (chunkX << 4)
-                            && centerX < (chunkX << 4) + 16
-                            && centerZ >= (chunkZ << 4)
-                            && centerZ < (chunkZ << 4) + 16)) {
-                        break;
-                    }
-                    if (block.getType() != area.getReplacement()) break;
-    		    
-                    SlimefunItemStack slimefunItemStack = generationInfo.getSlimefunItemStack();
-    		    
-                    if (slimefunItemStack.getType() != Material.PLAYER_HEAD) break;
-                    SkullMeta meta = (SkullMeta) slimefunItemStack.getItemMeta();
-                    PlayerProfile profile = meta.getPlayerProfile();
-                    BaseBlock skullBlock = itemToBlock(createSkullBaseBlock(profile));
-                	BlockVector3 pos = BlockVector3.at(centerX, centerY, centerZ);
-                	editSession.setBlock(pos, skullBlock);
-                    	
-                    BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController();
-                    if (controller.getBlockData(location) != null) {
-                        controller.removeBlock(location);
-                	}
-                        
-                    if (controller.getBlockData(location) == null) {
-                		controller.createBlock(location, slimefunItemStack.getItemId());
+        		for (int i = 0; i < area.getSize().getRandomBetween(random); i++) {
+    	            Location location = new Location(world, centerX, centerY, centerZ);
+    	            Block block = world.getBlockAt(centerX, centerY, centerZ);
+    	            if (!(centerX >= (chunkX << 4)
+    	                    && centerX < (chunkX << 4) + 16
+    	                    && centerZ >= (chunkZ << 4)
+    	                    && centerZ < (chunkZ << 4) + 16)) {
+    	                break;
+    	            }
+    	            if (block.getType() != area.getReplacement()) break;
+    	        
+    	            SlimefunItemStack slimefunItemStack = generationInfo.getSlimefunItemStack();
+    	        
+    	            BlockVector3 pos = BlockVector3.at(centerX, centerY, centerZ);
+    	            if (slimefunItemStack.getType() == Material.PLAYER_HEAD) {
+    	                SkullMeta meta = (SkullMeta) slimefunItemStack.getItemMeta();
+    	                PlayerProfile profile = meta.getPlayerProfile();
+    	                if (profile != null) {
+    	                    String texture = getTexture(profile);
+    	                    if (texture != null) {
+    	                        // 使用FAWE的NBT构建方式
+    	                        CompoundTag nbt = new CompoundTag(Map.of(
+    	                            "Id", new StringTag("minecraft:player_head"),
+    	                            "Properties", new CompoundTag(Map.of(
+    	                                "textures", new ListTag(CompoundTag.class, List.of(
+    	                                    new CompoundTag(Map.of(
+    	                                        "Value", new StringTag(texture)
+    	                                    ))
+    	                                ))
+    	                            ))
+    	                        ));
+    	                        
+    	                        // 创建带NBT的BaseBlock
+    	                        BaseBlock skullBlock = new BaseBlock(BlockTypes.PLAYER_HEAD.getDefaultState(), nbt);
+    	                        editSession.setBlock(pos, skullBlock);
+    	                    } else {
+    	                        editSession.setBlock(pos, BlockTypes.PLAYER_HEAD.getDefaultState());
+    	                    }
+    	                }
+    	            } else {
+    	                // 非头颅方块直接设置
+    	                BlockState blockState = BlockTypes.parse(slimefunItemStack.getType().name()).getDefaultState();
+    	                editSession.setBlock(pos, blockState);
+    	            }
+    	            
+    	            // 更新Slimefun数据库
+    	            BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController();
+    	            if (controller.getBlockData(location) != null) {
+    	                controller.removeBlock(location);
+    	            }
+    	            controller.createBlock(location, slimefunItemStack.getItemId());
+                    
+                    r = random.nextInt(0, 3);
+                    if (r == 0) {
+                        centerX++;
+                    } else if (r == 1) {
+                        centerY++;
+                    } else if (r == 2) {
+                        centerZ++;
                     }
                 }
                 editSession.flushQueue();
             } catch (Exception e) {
                 throw new RuntimeException("批量设置头颅失败", e);
             }
-        	
-        });
+    		
+    	});
+    	
+    	
         
     }
                 		
-		    
+    private String getTexture(PlayerProfile profile) {
+        if (profile == null) return null;
+        for (ProfileProperty property : profile.getProperties()) {
+            if ("textures".equals(property.getName())) {
+                return property.getValue();
+            }
+        }
+        return null;
+    }
                 
 }
